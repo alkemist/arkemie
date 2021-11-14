7b283344edd602094278501b08ff99c8178f8c90
Jérémy ACHAIN <achain.jeremy@gmail.com>
2021-11-14 19:15:29 +0100

    Update image carte lyon

--- carte-lyon.md
+++ carte-lyon.md
@@ -1,5 +1,7 @@
 ---
+title: Carte de Lyon avec Tramways en 1880
 layout: vuejs
+post-image: "/assets/images/cartes/1880-Plan_Lyon_Tramways_600.jpg"
 ---
 
 <carte-lyon></carte-lyon>
--- src/components/CarteLyon.vue
+++ src/components/CarteLyon.vue
@@ -2,7 +2,7 @@
   <v-zoomer :max-scale="maxScale" :zooming-elastic="zoomingElastic" :limit-translation="limitTranslation"
             style="width: 100vw; height: calc(100vh - 52px);">
     <img
-        src="/assets/images/cartes/1880-Plan_Lyon_Tramways_8000.jpg"
+        src="/assets/images/cartes/1880-Plan_Lyon_Tramways_8000_compress.webp"
         style="object-fit: contain; width: 100%; height: 100%;"
     >
   </v-zoomer>
