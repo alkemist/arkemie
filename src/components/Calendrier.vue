a6d57b6d991f3a20b4522ac634945693e8554419
Jérémy ACHAIN <achain.jeremy@gmail.com>
2021-11-24 17:52:02 +0100

    Amélioration du calendrier

--- src/components/Calendrier.vue
+++ src/components/Calendrier.vue
@@ -1,9 +1,33 @@
 <template>
-  <vue-cal class="vuecal--blue-theme" locale="fr" :time="false"
-      active-view="month" hide-view-selector :disable-views="['years', 'year', 'week', 'day']"
-           events-on-month-view="short"
-           :events="articles" :on-event-click="onEventClick" today-button
-  />
+  <div>
+    <vue-cal class="vuecal--blue-theme" locale="fr" :time="false"
+             active-view="month" hide-view-selector :disable-views="['years', 'year', 'week', 'day']"
+             events-on-month-view="short"
+             :events="articles" :on-event-click="onEventClick" today-button
+    />
+    <v-dialog v-model="showDialog">
+      <v-card>
+        <v-card-title>
+          <span v-html="selectedEvent.title"></span>
+        </v-card-title>
+        <v-divider></v-divider>
+        <v-card-text>
+          <p v-html="selectedEvent.content"></p>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+              color="primary"
+              text
+              v-bind:href="selectedEvent.url"
+              @click="dialog = false"
+          >
+            Voir l'article
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
@@ -11,6 +35,7 @@ import axios from 'axios';
 import VueCal from 'vue-cal'
 import 'vue-cal/dist/i18n/fr.js'
 import 'vue-cal/dist/vuecal.css'
+import 'vuetify/dist/vuetify.min.css'
 
 export default {
   components: { VueCal },
@@ -37,8 +62,8 @@ export default {
               start: article.date,
               end: article.date,
               title: article.title,
-              content: '',
-              class: ''
+              content: article.description,
+              url: article.url
             };
           });
         });
@@ -46,9 +71,28 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   .vuecal{
-    width: 100vw;
+    width: calc(100vw - 0.5vmax);
     height: calc(100vh - 52px);
   }
+  .vuecal__event{
+    left: 1px;
+    right: 1px;
+    background-color: rgba(76, 172, 175, 0.35);
+  }
+  .vuecal--short-events .vuecal__event-title{
+    white-space: normal;
+  }
+  .vuecal--month-view .vuecal__cell-content {
+    justify-content: flex-start;
+    height: 100%;
+    align-items: flex-end;
+  }
+  hr{
+    margin: 0 auto 1rem;
+  }
+
+  .vuecal--month-view .vuecal__cell-date {padding: 4px;}
+  .vuecal--month-view .vuecal__no-event {display: none;}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import VueZoomer from 'vue-zoomer'
 import CarteLyon from './components/CarteLyon'
 import Calendrier from './components/Calendrier';
+import Vuetify from 'vuetify'
 
 Vue.use(VueZoomer)
+Vue.use(Vuetify)
 Vue.component('CarteLyon', CarteLyon)
 Vue.component('Calendrier', Calendrier)
 
 new Vue({
     el: '#app',
+    vuetify: new Vuetify(),
 })
